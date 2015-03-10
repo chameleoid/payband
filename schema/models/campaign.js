dd62614058e5d425a65a432cd4cef4861413138d
rummik <r@9k1.us>
2015-03-09 23:03:03 -0400

    Add campaign schema

---
+++ schema/models/campaign.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('Campaign', {
+    uuid: {
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+    },
+
+    title: DataTypes.STRING,
+
+    goal: DataTypes.INTEGER,
+    current: DataTypes.INTEGER,
+
+    startdate: DataTypes.DATE,
+    enddate: DataTypes.DATE,
+  });
+};
