52d9b0c95b57f6d9f695301910d21ccb9824d58d
rummik <r@9k1.us>
2015-03-12 09:21:06 -0400

    Add comments to user schema

--- schema/models/user.js
+++ schema/models/user.js
@@ -6,10 +6,16 @@ module.exports = function(sequelize, DataTypes) {
       defaultValue: DataTypes.UUIDV1,
     },
 
+    // Email address  (login)
     email: DataTypes.STRING,
+
+    // Hashed value of passphrase / password
     passphrase: DataTypes.STRING,
 
+    // User's display name
     username: DataTypes.STRING,
+
+    // Stripe token used for billing
     stripeToken: DataTypes.STRING,
   });
 };
