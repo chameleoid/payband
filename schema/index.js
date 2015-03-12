ea3b2db4c2b39813daa65863705e69c6072ba8ae
rummik <r@9k1.us>
2015-03-12 09:10:15 -0400

    Clean up schema index slightly

--- schema/index.js
+++ schema/index.js
@@ -2,18 +2,21 @@ var Sequelize = require('sequelize');
 var db = new Sequelize(process.env.DATABASE_URL);
 
 var User = db.import(__dirname + '/models/user.js');
+
 var Campaign = db.import(__dirname + '/models/campaign.js');
 var Project = db.import(__dirname + '/models/project.js');
-var Payment = db.import(__dirname + '/models/payment.js');
+
 var Goal = db.import(__dirname + '/models/goal.js');
 
+var Payment = db.import(__dirname + '/models/payment.js');
+
 Payment.hasOne(User);
+
+Campaign.hasMany(Goal);
+Project.hasMany(Goal);
 
 User.hasMany(Payment);
 Campaign.hasMany(Payment);
 Project.hasMany(Payment);
-
-Campaign.hasMany(Goal);
-Project.hasMany(Goal);
 
 module.exports = db;
