64d977c1619ee49c2608c5326532c17e90a25abb
rummik <r@9k1.us>
2015-03-12 09:19:02 -0400

    Add comments to project schema

--- schema/models/project.js
+++ schema/models/project.js
@@ -6,12 +6,19 @@ module.exports = function(sequelize, DataTypes) {
       defaultValue: DataTypes.UUIDV1,
     },
 
+    // Project title / name
     title: DataTypes.STRING,
+
+    // Description of project
     description: DataTypes.STRING,
 
+    // Income goal of billing interval
     goal: DataTypes.INTEGER,
+
+    // Current value of transactional income per billing interval
     current: DataTypes.INTEGER,
 
+    // Billing interval (in days)
     interval: DataTypes.INTEGER,
   });
 };
