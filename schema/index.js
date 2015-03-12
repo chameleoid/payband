92ae73e5f0f67a08f82d0d0f32f5b3784768a5d2
rummik <r@9k1.us>
2015-03-11 20:02:15 -0400

    Wire schema a bit more

--- schema/index.js
+++ schema/index.js
@@ -3,10 +3,17 @@ var db = new Sequelize(process.env.DATABASE_URL);
 
 var User = db.import(__dirname + '/models/user.js');
 var Campaign = db.import(__dirname + '/models/campaign.js');
+var Project = db.import(__dirname + '/models/project.js');
 var Payment = db.import(__dirname + '/models/payment.js');
+var Goal = db.import(__dirname + '/models/goal.js');
 
 Payment.hasOne(User);
+
 User.hasMany(Payment);
 Campaign.hasMany(Payment);
+Project.hasMany(Payment);
+
+Campaign.hasMany(Goal);
+Project.hasMany(Goal);
 
 module.exports = db;
